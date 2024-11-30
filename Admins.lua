a79fa57f98d438bec4baf895c19740f9c5a69051
Tariq <49447901+3isht@users.noreply.github.com>
2024-11-30 11:45:38 +0300

    Create Admins.lua

---
+++ Admins.lua
@@ -0,0 +1,15 @@
+local Admins = {
+    5664216985, -- Jathooom
+    2792624188, -- 8D_Mh
+    360164176, -- lmcj1
+    2017968053, -- iN3seer
+    1801382761, -- Afixarius
+    498096109, -- mohmadmuin
+    526166088, -- MadIyHated
+    1151917188, -- 006I7
+    4798578151, -- ziad_amr15
+    5641542715, -- ziad_amr151
+    1794796236 -- SouIisDeath
+}
+
+return Admins
